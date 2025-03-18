fe0669918e9aebc465ebc1f8d296d102e239906a
PPR-prog <wiranggga24@gmail.com>
2025-03-18 07:26:24 -0700

    aca

--- Metalgear/routes/web.php
+++ Metalgear/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
-Route::get('/welcome', function () {
-    return view('welcome');
-});
+//Route::get('/welcome', function () {
+//    return view('welcome');
+//});
+
+
+Route::get('/', [HomeController::class,'index']);
+Route::get('/contact', [HomeController::class,'contact']);
